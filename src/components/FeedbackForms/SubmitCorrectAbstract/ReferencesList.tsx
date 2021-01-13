3481dfa0814aca646fcc3843b366300d631e2460
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2021-01-13 16:54:32 -0500

    Disallow adding new references when editing record

--- src/components/FeedbackForms/SubmitCorrectAbstract/ReferencesList.tsx
+++ src/components/FeedbackForms/SubmitCorrectAbstract/ReferencesList.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { useFormContext, useFieldArray } from 'react-hook-form';
 import FlexView from 'react-flexview';
+import { useFieldArray, useFormContext } from 'react-hook-form';
 import styled from 'styled-components';
+import { Control, SelectControl } from '../components';
 import {
+  EntryType,
+  Reference,
   referenceOptions,
   SubmitCorrectAbstractFormValues,
-  Reference,
-  EntryType,
 } from '../models';
-import { Control, SelectControl } from '../components';
 
 interface IControlRow {
   count: number;
@@ -35,6 +35,7 @@ const ReferencesList: React.FC = () => {
   const { control, register, errors, getValues } = useFormContext<
     SubmitCorrectAbstractFormValues
   >();
+
   const { fields, append, remove } = useFieldArray<Reference>({
     control,
     name,
@@ -71,56 +72,70 @@ const ReferencesList: React.FC = () => {
 
   return (
     <ControlContainer>
-      {fields.map(({ id, type, value }, index) => {
-        return (
-          <ControlRow key={id} count={entryType === EntryType.Edit ? 2 : 1}>
-            {entryType === EntryType.Edit && (
-              <SelectControl
-                field={`${name}[${index}].type`}
-                label={`Reference ${index + 1} type`}
-                a11yPrefix="feedback"
-                ref={register()}
-                defaultValue={type}
-                errorMessage={getErrorMessage(index, name, 'type')}
-              >
-                {[
-                  <option value="none" key="none">
-                    Choose a type
-                  </option>,
-                  ...referenceOptions.map(({ key, text }) => (
-                    <option value={key} key={key}>
-                      {text}
-                    </option>
-                  )),
-                ]}
-              </SelectControl>
-            )}
-            <Control
-              field={`${name}[${index}].value`}
-              type="text"
-              a11yPrefix="feedback"
-              label={`Reference ${index + 1}`}
-              defaultValue={value}
-              errorMessage={getErrorMessage(index, name, 'value')}
-              actionButton={
-                <button
-                  disabled={fields.length <= 1}
-                  className="btn btn-danger"
-                  onClick={() => handleRemove(index)}
+      {fields.length === 1 && entryType === EntryType.Edit ? (
+        <p>No References</p>
+      ) : (
+        fields.map(({ id, type, value }, index) => {
+          return (
+            <ControlRow key={id} count={entryType === EntryType.Edit ? 2 : 1}>
+              {entryType === EntryType.Edit && (
+                <SelectControl
+                  field={`${name}[${index}].type`}
+                  label={`Reference ${index + 1} type`}
+                  a11yPrefix="feedback"
+                  ref={register()}
+                  defaultValue={type}
+                  errorMessage={getErrorMessage(index, name, 'type')}
                 >
-                  <i className="fa fa-trash" aria-hidden="true" />
-                  <span className="sr-only">Remove</span>
-                </button>
-              }
-              ref={register()}
-            />
-          </ControlRow>
-        );
-      })}
+                  {[
+                    <option value="none" key="none">
+                      Choose a type
+                    </option>,
+                    ...referenceOptions.map(({ key, text }) => (
+                      <option value={key} key={key}>
+                        {text}
+                      </option>
+                    )),
+                  ]}
+                </SelectControl>
+              )}
+              <Control
+                field={`${name}[${index}].value`}
+                type="text"
+                a11yPrefix="feedback"
+                label={`Reference ${index + 1}`}
+                defaultValue={value}
+                errorMessage={getErrorMessage(index, name, 'value')}
+                actionButton={
+                  <button
+                    disabled={fields.length <= 1}
+                    className="btn btn-danger"
+                    onClick={() => handleRemove(index)}
+                  >
+                    <i className="fa fa-trash" aria-hidden="true" />
+                    <span className="sr-only">Remove</span>
+                  </button>
+                }
+                ref={register()}
+              />
+            </ControlRow>
+          );
+        })
+      )}
       <FlexView hAlignContent="left">
-        <button type="button" className="btn btn-default" onClick={handleAdd}>
-          <i className="fa fa-plus" aria-hidden="true" /> Add new reference
-        </button>
+        {entryType === EntryType.Edit ? (
+          <p>
+            Please use the{' '}
+            <a href="/feedback/missingreferences">
+              Add Missing References form
+            </a>{' '}
+            for adding missing references.
+          </p>
+        ) : (
+          <button type="button" className="btn btn-default" onClick={handleAdd}>
+            <i className="fa fa-plus" aria-hidden="true" /> Add new reference
+          </button>
+        )}
       </FlexView>
     </ControlContainer>
   );
