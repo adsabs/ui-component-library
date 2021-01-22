30f089e1c7158a2967cbf53f9818e4d68d5afe37
Tim Hostetler <6970899+thostetler@users.noreply.github.com>
2021-01-22 13:53:07 -0500

    add shallowEqual util

---
+++ src/utils/shallowEqual.ts
@@ -0,0 +1,36 @@
+/**
+ *
+ * @see https://github.com/moroshko/shallow-equal
+ * @param {object} objA
+ * @param {object} objB
+ */
+export default function shallowEqual(objA: object, objB: object) {
+  if (objA === objB) {
+    return true;
+  }
+
+  if (!objA || !objB) {
+    return false;
+  }
+
+  var aKeys = Object.keys(objA);
+  var bKeys = Object.keys(objB);
+  var len = aKeys.length;
+
+  if (bKeys.length !== len) {
+    return false;
+  }
+
+  for (var i = 0; i < len; i++) {
+    var key = aKeys[i];
+
+    if (
+      objA[key] !== objB[key] ||
+      !Object.prototype.hasOwnProperty.call(objB, key)
+    ) {
+      return false;
+    }
+  }
+
+  return true;
+}
